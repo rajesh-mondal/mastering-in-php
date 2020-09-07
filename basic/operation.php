7ee2f19a53b377ec5993e85f57e3753b39f34bf1
Rajesh Mondal <rajesh01749@gmail.com>
2020-09-07 15:23:20 +0600

    basic operation

---
+++ basic/operation.php
@@ -0,0 +1,37 @@
+<?php
+
+$number1 = 10;
+$number2 = 5;
+
+$add = $number1 + $number2;
+$sub = $number1 - $number2;
+$mul = $number1 * $number2;
+$div = $number1 / $number2;
+$mod = $number1 % $number2;
+
+echo "Addition is {$add} \n";
+echo "Substraction is {$sub} \n";
+echo "Multiplication {$mul} \n";
+echo "Division is {$div} \n";
+echo "Modulation is {$mod}";
+
+
+
+/*
+$number = 12;
+$number++; //$number = +=1; $number = $number +1;
+$number--;
+
+//$number += 13;
+//$number2 = $number - 20;
+$division = 13/4;
+$module = 12%5;
+
+//$number = (24-4) / (7-2);
+//$number /=5;
+
+echo "Division is " .$division;
+echo "\n";
+//echo "Module is " .$module;
+echo $number;
+ */
