a553864e7885d03ecec09d2ba360f014f6b80711
Rajesh Mondal <rajesh01749@gmail.com>
2020-09-07 15:30:43 +0600

    number system

---
+++ basic/number.php
@@ -0,0 +1,19 @@
+<?php
+/*
+$n = 7;
+$m = $n++;
+echo $m, "\n", $n;
+// $m= $n++; $m = $n; $n = $n+1;
+ */
+
+$n = 12;
+$o = 017;
+$h = 0x1B;
+
+//printf("The number is %d, %d and %d",$n, $o, $h );
+printf( "The Binary equivalent of %d is %b", 1212, 1212 );
+printf( "\nThe Hexadecimal equivalent of %d is %X", 1212, 1212 );
+printf( "\nThe octal equivalent of %d is %o", 1212, 1212 );
+
+printf( "\nThe octal equivalent of %d is %o", 65536, 65536 );
+printf( "\nOcatal %o = %d", 0200000, 0200000 );
