8e2b2068be2a93ce048f33fbb3676a9e2aabc80a
Rajesh Mondal <rajesh01749@gmail.com>
2020-09-08 14:05:42 +0600

    nested if else

---
+++ basic/nestedif.php
@@ -0,0 +1,31 @@
+<?php
+// nested if else
+$condition1 = true;
+$condition2 = true;
+$condition3 = false;
+
+if ( $condition1 ) {
+    if ( $condition2 ) {
+        if ( $condition3 ) {
+            echo "Hello";
+        } else {
+            echo "no 1";
+        }
+    } else {
+        echo "no 2";
+    }
+} else {
+    echo "no3";
+}
+
+echo "\n";
+
+if ( $condition1 && $condition2 && $condition3 ) {
+    echo "Hello";
+} elseif ( $condition1 && $condition2 ) {
+    echo "no 1";
+} elseif ( $condition1 ) {
+    echo "no 2";
+} else {
+    echo "no 3";
+}
