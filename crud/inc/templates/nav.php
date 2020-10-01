7cc4023f3477efed30df781e16b4e694a3bfeb8f
Rajesh Mondal <rajesh01749@gmail.com>
2020-10-01 14:05:39 +0600

    crud project

---
+++ crud/inc/templates/nav.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+?>
+<div style="margin-top: 20px;">
+    <div class="float-left">
+        <p>
+            <a href="index.php?task=report">All Students</a>
+            <?php if ( hasPrivilege() ): ?>
+                |
+                <a href="index.php?task=add">Add New Student</a>
+            <?php endif;?>
+            <?php if ( isAdmin() ): ?>
+                |
+                 <a href="index.php?task=seed">Seed</a>
+            <?php
+endif;
+?>
+        </p>
+    </div>
+    <div class="float-right">
+        <?php
+if ( !$_SESSION['loggedin'] ):
+?>
+        <a href="auth.php">Log In</a>
+        <?php
+else:
+?>
+        <a href="auth.php?logout=true">Log Out (<?php echo $_SESSION['role'] ?>)</a>
+        <?php
+endif;
+?>
+    </div>
+    <p></p>
+</div>
