cbc520cf49d4aed3b3b7b4cf9d17597e916b84a8
Rajesh Mondal <rajesh01749@gmail.com>
2021-01-15 23:53:15 +0600

    adapter pattern

---
+++ design pattern/adapter.php
@@ -0,0 +1,44 @@
+<?php
+
+interface PaymentGateway {
+    function sendPayment( $payment );
+}
+
+class PaymentProcessor {
+    private $gateway;
+    function __construct( PaymentGateway $pg ) {
+        $this->gateway = $pg;
+    }
+
+    function purchaseProduct( $amount ) {
+        $this->gateway->sendPayment( $amount );
+    }
+}
+
+class Paypal implements PaymentGateway {
+    function sendPayment( $payment ) {
+        echo "{$payment} processed by Paypal";
+    }
+}
+
+class Stripe {
+    function makePayment( $amount, $currency = null ) {
+        echo "{$amount} processed by Stripe";
+    }
+}
+
+class StripeAdapter implements PaymentGateway {
+    private $stripe;
+    function __construct( Stripe $stripe ) {
+        $this->stripe = $stripe;
+    }
+    function sendPayment( $payment ) {
+        $this->stripe->makePayment( $payment );
+    }
+}
+
+$paypal = new Paypal();
+$stripe = new Stripe();
+$sa = new StripeAdapter( $stripe );
+$pp = new PaymentProcessor( $paypal );
+$pp->purchaseProduct( 45 );
